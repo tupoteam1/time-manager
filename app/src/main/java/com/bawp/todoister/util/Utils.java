9f7b7fa2f42a597323a2e0e3580dd7f3b989a01c
RReskk <zyxorus@mail.ru>
2021-10-03 17:03:25 +0500

    aga

--- app/src/main/java/com/bawp/todoister/util/Utils.java
+++ app/src/main/java/com/bawp/todoister/util/Utils.java
@@ -6,7 +6,7 @@ import java.util.Date;
 public class Utils {
     public static String formatDate(Date date) {
         SimpleDateFormat simpleDateFormat = (SimpleDateFormat) SimpleDateFormat.getDateInstance();
-        simpleDateFormat.applyPattern("EEE, MMM d");
+        simpleDateFormat.applyPattern("EEE, d MMM");
         return simpleDateFormat.format(date);
     }
 }
